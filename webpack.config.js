2258284f96fa9dda0212dfcd70bfe3abb28d636c
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-10-23 23:07:16 +0300

    first commit webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+      },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader',
+              },
+            ],
+          },
+          {
+            test: /\.css$/,
+            use: [
+              MiniCssExtractPlugin.loader, 'css-loader',
+            ],
+          }
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: './src/index.html',
+          filename: "./main.html"
+        }),
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+        })
+      ],
+}
